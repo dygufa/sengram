2e1f0bbcaa3406c7f0425cec2a7760e4aa4a22b9
Rodrigo Araujo <dygufa@gmail.com>
2018-04-04 01:16:28 -0300

    formatting message as markdown

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,6 +1,12 @@
-import TelegramBot from "node-telegram-bot-api";
+import * as TelegramBot from "node-telegram-bot-api";
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
 export function sendMessage(message: string) {
-    const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
-    bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message);
+    bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message, {
+        parse_mode: "Markdown"
+    }).then(res => {
+        console.log(res);
+    }).catch(e => {
+        console.error(e);
+    });
 }
