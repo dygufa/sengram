247e334302f1fe406d0e161ed9fa2b905c6b3aec
Rodrigo Araujo <dygufa@gmail.com>
2018-04-04 01:26:00 -0300

    making sure env variables are set

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,8 +1,18 @@
 import * as TelegramBot from "node-telegram-bot-api";
+
+if (!process.env.TELEGRAM_BOT_TOKEN) {
+    throw new Error("TELEGRAM_BOT_TOKEN not defined");
+}
+
+if (!process.env.TELEGRAM_CHAT_ID) {
+    throw new Error("TELEGRAM_CHAT_ID not defined");
+}
+
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
+
 export function sendMessage(message: string) {
-    bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message, {
+    bot.sendMessage(process.env.TELEGRAM_CHAT_ID!, message, {
         parse_mode: "Markdown"
     }).then(res => {
         console.log(res);
