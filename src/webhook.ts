b4a36c9dac4182663087ce510eeba2e6894ff8a9
Rodrigo Araujo <dygufa@gmail.com>
2018-12-25 19:42:11 -0300

    sanitizing telegram message to prevent markdown conflicts

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import * as telegram from "./telegram";
 
+function sanitize(message: string) {
+    return message.replace(/(\*|\_)/g, x => `\\${x}`);
+}
+
 export default function webhook(req: Request, res: Response) {
     const body = req.body;
 
-    const message = `*Projeto*: ${body.project_name} \n*Mensagem*: ${body.message} \n*Url*: ${body.url}`;
+    const message = `*Projeto*: ${sanitize(body.project_name)} \n*Mensagem*: ${sanitize(body.message)} \n*Url*: ${body.url}`;
 
     telegram.sendMessage(message);
     res.end();
